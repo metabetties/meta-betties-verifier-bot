6642a98cbfd015084e69bea0a9840931f20468af
Jason Pieroni <macbookpro2023@Jasons-MacBook-Pro.local>
2025-07-14 12:40:37 -0400

    Fix: Secure bot token and Helius API key using env vars

--- Downloads/meta-betties-verifier-bot/index.js
+++ Downloads/meta-betties-verifier-bot/index.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json());Telegraf
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const PORT = process.env.PORT || 8080;
